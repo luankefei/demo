ffdbf5076bccc4f3e02b1440c3a7c6be9246993c
luankefei <luankefei@gmail.com>
2015-05-27 13:05:17 +0800

    change tab to space

--- javascript/module-test.js
+++ javascript/module-test.js
@@ -1,77 +1,77 @@
 var dropdown = (function() {
 
-	var module = {
-	    node: null,
-		dataset: null,
-		init: function(node) {
+    var module = {
+        node: null,
+        dataset: null,
+        init: function(node) {
 
-			module.node = node
+            module.node = node
 
-			module.data.init(function() {
+            module.data.init(function() {
 
-				module.view.init(node)
-				module.controller.init()
-			})
-		}
-	}
+                module.view.init(node)
+                module.controller.init()
+            })
+        }
+    }
     
     // 定义模板，初始化模板
-	module.view = {
+    module.view = {
 
-		html: '<div><ul>fs-replace</ul></div>',
-		init: function(node) {
+        html: '<div><ul>fs-replace</ul></div>',
+        init: function(node) {
     
             // TODO: 增加格式校验
-			var html = ''
-			var data = module.dataset
+            var html = ''
+            var data = module.dataset
 
-			for (var i = 0, length = data.length; i < length; i++) {
+            for (var i = 0, length = data.length; i < length; i++) {
 
-				html += '<li>' + data[i] + '</li>'
-			}
+                html += '<li>' + data[i] + '</li>'
+            }
 
-			module.view.html = module.view.html.replace('fs-replace', html)
+            module.view.html = module.view.html.replace('fs-replace', html)
 
-			node.innerHTML = module.view.html
-		}
-	}
+            node.innerHTML = module.view.html
+        }
+    }
 
     // 事件，数据逻辑
-	module.controller = {
+    module.controller = {
 
-		init: function() {
-			// 初始化view层	
-			module.view.init(module.dataset)
-			// 绑定事件
-			module.controller.registerEvent(module.node)
-		},
+        init: function() {
+            // 初始化view层 
+            module.view.init(module.dataset)
+            // 绑定事件
+            module.controller.registerEvent(module.node)
+        },
 
-		registerEvent: function(node) {
+        registerEvent: function(node) {
 
-			// 点击时切换下拉列表的隐藏、显示 	   
-			node.addEventListener('click', function() {
+            // 点击时切换下拉列表的隐藏、显示     
+            node.addEventListener('click', function() {
 
-				alert(node.getAttribute('id') + '  clicked')
+                alert(node.getAttribute('id') + '  clicked')
 
-			},
-			false)
+            },
+            false)
 
-		}
-	}
+        }
+    }
     
     // 数据读取、存储
-	module.data = {
+    module.data = {
 
-		init: function(callback) {
+        init: function(callback) {
 
-			var url = '/people'
-			var d = ['男', '女']
+            var url = '/people'
+            var d = ['男', '女']
 
-			module.dataset = d
-			callback && callback()
-		}
-	}
+            module.dataset = d
+            callback && callback()
+        }
+    }
 
-	return module
+    return module
 })()
 
